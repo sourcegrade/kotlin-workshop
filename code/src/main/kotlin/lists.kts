7f2a6a6b7e14d8dec6d0307f6a2b0bdfc88c4441
Alexander Städing <alexanderstaeding@gmail.com>
2022-10-13 15:03:33 +0200

    Fix lists.kts

--- code/src/main/kotlin/lists.kts
+++ code/src/main/kotlin/lists.kts
@@ -1,4 +1,4 @@
 val list = listOf("foo", "bar", "baz")
 // list.add("qux") // geht nicht
 val mutableList = mutableListOf("foo", "bar", "baz")
-list.add("qux") // geht
+mutableList.add("qux") // geht
